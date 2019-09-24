9cf6f423bf5cd98260a9b2907be55bbaa3a0ccb6
DaiDP <phuocdaivl08@gmail.com>
2019-09-24 11:28:21 +0700

    support .env config

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'connection_string' => 'Endpoint=sb://daidp-push.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=KoYAnCwRuYSUQvFms/Yq+C8G7xaupllgol/6lNHPJT4=',
-    'hub_path'  => 'daidp-push/Tutorial',
-    'token_ttl' => 60
+    'connection_string' => env('AZ_CONNECTION_STRING'),
+    'hub_path'  => env('AZ_HUB_PATH'),
+    'token_ttl' => env('AZ_AUTHORIZATION_TTL', 60)
 ];
